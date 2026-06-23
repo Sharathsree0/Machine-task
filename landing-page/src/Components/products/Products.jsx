e4e7840447250474b713faffb807cf90d4f29ce1
sharath <sharathsree916@gmail.com>
2026-06-23 21:25:48 +0530

    small

--- landing-page/src/Components/products/Products.jsx
+++ landing-page/src/Components/products/Products.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { API_URL } from "../config/api.js";
+import { API_URL } from "../../config/api.js";
 
 function Products() {
   const [products, setProducts] = useState([]);
